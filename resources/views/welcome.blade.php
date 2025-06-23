78fad3bac0a3f2791991a9437a380bbe3c2ad7cf
RakaDr <randikaraka19@gmail.com>
2025-06-24 00:04:20 +0700

    Up Foto Raka

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -219,7 +219,7 @@
                 </div>
                 <div class="grid grid-cols-1 md:grid-cols-5 gap-10">
                     <div class="bg-[#F65500]/20 border-[#F65500]/50 hover:border-[#F65500] p-6 rounded-xl border hover:scale-105 transition">
-                        <img src="{{ asset('images/') }}" alt="" class="rounded-2xl w-32 h-32 mx-auto mb-4 ">
+                        <img src="{{ asset('images/raka.jpg') }}" alt="" class="rounded-2xl w-32 h-32 mx-auto mb-4 ">
                         <h3 class="text-xl text-white font-semibold mb-3">Raka Dwi Randika</h3>
                         <p class="text-white/60">0110224109</p>
                     </div>
