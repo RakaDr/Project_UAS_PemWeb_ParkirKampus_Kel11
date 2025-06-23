268afb19bb9afec4539615f3d26d83bb7698eb20
Roisazzshid3 <isamiq3@gmail.com>
2025-06-23 17:46:13 +0700

    update welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -359,9 +359,6 @@
 
                 <div class="mt-8 border-t border-gray-100 pt-8 dark:border-gray-800">
                 <div class="sm:flex sm:justify-between">
-                    <p class="text-xs text-white dark:text-gray-400">
-                    &copy; 2022. Company Name. All rights reserved.
-                    </p>
 
                     <ul class="mt-8 flex flex-wrap justify-start gap-4 text-xs sm:mt-0 lg:justify-end">
                     <li>
